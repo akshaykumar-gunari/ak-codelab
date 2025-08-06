39a526671152207bf8d7b4dc89d33ae76fe22b50
akshaykumar-gunari <akshaygunari@gmail.com>
2025-08-06 14:05:46 +0530

    Add daily-challenge August 06th 2025

--- daily-challenge/2025/Aug/two_sum.py
+++ daily-challenge/2025/Aug/two_sum.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-File: file_name.py
-Description: <Description>
+File: two_sum.py
+Description: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 Author: Akshaykumar Gunari
-Created: 2025-03-04
-Last Modified: 2025-03-04
+Created: 2025-08-06
+Last Modified: 2025-08-06
 Version: 1.0.0
 Python: 3.8+
 
-Detailed description in one to two lines.
+This script outputs the indices of two numbers in an array whose sum is equal to the given target.
 
 Execution: python file_name.py
 
@@ -31,9 +31,17 @@ sys.path.insert(0, get_git_root())
 import python_utils
 from python_utils import ak_utils
 
+def two_sum(arr, target):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] + arr[j] == target:
+                return [i, j]
+
 def main():
-    python_utils.__status__ = "Under Development"
+    ak_utils.__status__ = "Completed"
     ak_utils.print_provenance()
+    indices = two_sum([2, 7, 11, 15], 9)
+    print(indices)
 
 if __name__ == "__main__":
     main()
